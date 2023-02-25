1bc684a34eb8181080feb741711ecc01a3779627
Musfakur <procreationsbliss@gmail.com>
2023-02-25 19:58:03 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      'white': '#ffffff',
+      'text-blue': '#355EFC',
+      'btn-text-blue': '#dfe4fd',
+      'btn-hover-blue': '#2a4bca',
+      'heading-blue': '#011A41',
+      'gray': '#555',
+    },
+    fontFamily: {
+      jost: ['Jost', 'sans-serif'],
+      sans: ['Open Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
