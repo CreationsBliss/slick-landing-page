7ebad8ba2ec3c187eaf07bfb0603cba039fdb694
Musfakur <procreationsbliss@gmail.com>
2023-02-25 23:46:55 +0600

    footer section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ module.exports = {
       'white': '#ffffff',
       'text-blue': '#355EFC',
       'btn-text-blue': '#dfe4fd',
+      'footer-top-blue': '#011A41',
       'btn-hover-blue': '#2a4bca',
       'heading-blue': '#011A41',
-      'gray': '#555',
+      'gray': '#555555',
       'light-gray': '#F4F6F6',
+      'footer-blue': '#000B1C',
     },
     fontFamily: {
       jost: ['Jost', 'sans-serif'],
