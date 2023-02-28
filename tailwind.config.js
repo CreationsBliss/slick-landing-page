7e9e818a72cb76935d4216765f7831c0317df4eb
Musfakur <procreationsbliss@gmail.com>
2023-03-01 00:59:31 +0600

    form section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       'white': '#ffffff',
       'text-blue': '#355EFC',
       'btn-text-blue': '#dfe4fd',
-      'footer-top-blue': '#011A41',
+      'footer-top-blue': '#011A41', 
       'btn-hover-blue': '#2a4bca',
       'heading-blue': '#011A41',
       'gray': '#555555',
