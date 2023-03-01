f9828728c50fe4cb8587568774d5054f46037a8e
Musfakur <procreationsbliss@gmail.com>
2023-03-02 01:59:24 +0600

    responsiveness done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,29 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    colors: {
-      'white': '#ffffff',
-      'text-blue': '#355EFC',
-      'btn-text-blue': '#dfe4fd',
-      'footer-top-blue': '#011A41', 
-      'btn-hover-blue': '#2a4bca',
-      'heading-blue': '#011A41',
-      'gray': '#555555',
-      'light-gray': '#F4F6F6',
-      'footer-blue': '#000B1C',
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'white': '#ffffff',
+        'text-blue': '#355EFC',
+        'btn-text-blue': '#dfe4fd',
+        'footer-top-blue': '#011A41', 
+        'btn-hover-blue': '#2a4bca',
+        'heading-blue': '#011A41',
+        'gray': '#555555',
+        'light-gray': '#F4F6F6',
+        'footer-blue': '#000B1C',
+      },
+      fontFamily: {
+        jost: ['Jost', 'sans-serif'],
+        sans: ['Open Sans', 'sans-serif'],
+      },
     },
-    fontFamily: {
-      jost: ['Jost', 'sans-serif'],
-      sans: ['Open Sans', 'sans-serif'],
-    },
-    extend: {},
   },
   plugins: [],
 }
